f05c6cd2a106cf461c85a8bf853aa8f65a601834
SOUnit <workbookwork7@gmail.com>
2023-10-26 20:21:31 -0700

    add sample

---
+++ 01-scroll-active-navigation/sample-data.js
@@ -0,0 +1,54 @@
+// category - products
+var product = {
+  name: "product name",
+};
+
+var category = {
+  name: "category name",
+  products: [
+    product,
+    product,
+    product,
+    product,
+    product,
+    product,
+    product,
+    product,
+    product,
+    product,
+  ],
+};
+
+var sampleData = [
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+  category,
+];
+
+console.log(sampleData.length);
